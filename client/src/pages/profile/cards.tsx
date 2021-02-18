379ed72a8f17483ec03b36289095363b5666f18a
kukajp3 <kukajp@gmail.com>
2021-02-18 19:04:38 -0300

    create Profile page

---
+++ client/src/pages/profile/cards.tsx
@@ -0,0 +1,20 @@
+import Profile from 'templates/Profile'
+import CardsList, { CardsListProps } from 'components/CardsList'
+
+import cardsMock from 'components/PaymentOptions/mock'
+
+export default function Cards({ cards }: CardsListProps) {
+  return (
+    <Profile>
+      <CardsList cards={cards} />
+    </Profile>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: cardsMock
+    }
+  }
+}
